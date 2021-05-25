a5a8773ee6172daa33369e4984e3225e621ee6e4
garrathjones <jones.garrath@gmail.com>
2021-05-25 21:25:40 +0100

    First commit - (empty) classes for framework created and basic test created

---
+++ EATestProject/EAEmployeeTest/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace EAEmployeeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string url = "http://eaapp.somee.com";
+
+        private IWebDriver _driver;
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl(url);
+
+            Login();
+        }
+
+        public void Login()
+        {
+            //click "Log in"
+            _driver.FindElement(By.LinkText("Login")).Click();
+            //enter username
+            _driver.FindElement(By.Id("UserName")).SendKeys("admin");
+            //enter password
+            _driver.FindElement(By.Id("Password")).SendKeys("password");
+            //click login
+            _driver.FindElement(By.CssSelector(".btn-default")).Submit();
+            
+        }
+    }
+}
