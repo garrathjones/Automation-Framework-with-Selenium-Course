f8a4d1f9927834b5632e1187aee9ccc54badebbc
garrathjones <jones.garrath@gmail.com>
2021-05-25 22:57:53 +0100

    Updated test to use page object model, created LoginPage.cs

---
+++ EATestProject/EAEmployeeTest/Pages/LoginPage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+
+namespace EAEmployeeTest.Pages
+{
+    class LoginPage
+    {
+        //Initialise login page
+        public LoginPage(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+        
+        //Objects for login page
+        [FindsBy(How = How.LinkText, Using = "Login")]
+        public IWebElement lnkLogin { get; set; }
+
+        [FindsBy(How = How.Id, Using = "UserName")]
+        public IWebElement txtUserName { get; set; }
+
+        [FindsBy(How = How.Id, Using = "Password")]
+        public IWebElement txtPassword { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = ".btn-default")]
+        public IWebElement btnLogin { get; set; }
+    }
+}
--- EATestProject/EAEmployeeTest/UnitTest1.cs
+++ EATestProject/EAEmployeeTest/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using EAEmployeeTest.Pages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
@@ -8,31 +9,33 @@ namespace EAEmployeeTest
     [TestClass]
     public class UnitTest1
     {
-        string url = "http://eaapp.somee.com";
-
+        string url = "http://eaapp.somee.com";        
         private IWebDriver _driver;
-
 
         [TestMethod]
         public void TestMethod1()
         {
             _driver = new ChromeDriver();
             _driver.Navigate().GoToUrl(url);
-
             Login();
         }
 
         public void Login()
         {
-            //click "Log in"
-            _driver.FindElement(By.LinkText("Login")).Click();
-            //enter username
-            _driver.FindElement(By.Id("UserName")).SendKeys("admin");
-            //enter password
-            _driver.FindElement(By.Id("Password")).SendKeys("password");
-            //click login
-            _driver.FindElement(By.CssSelector(".btn-default")).Submit();
-            
+            ////click link for "Log in" Page
+            //_driver.FindElement(By.LinkText("Login")).Click();
+            ////enter username
+            //_driver.FindElement(By.Id("UserName")).SendKeys("admin");
+            ////enter password
+            //_driver.FindElement(By.Id("Password")).SendKeys("password");
+            ////click login
+            //_driver.FindElement(By.CssSelector(".btn-default")).Submit();
+
+            LoginPage page = new LoginPage(_driver);
+            page.lnkLogin.Click();
+            page.txtUserName.SendKeys("admin");
+            page.txtPassword.SendKeys("password");
+            page.btnLogin.Submit();      
         }
     }
 }
