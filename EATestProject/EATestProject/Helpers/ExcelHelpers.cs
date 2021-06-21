b2f5515a527e5c0a4b37f1df591fc93c30e77cbb
garrathjones <jones.garrath@gmail.com>
2021-06-21 14:38:37 +0100

    Added code to ExcelHelper: 1. Created a method to read data from an Excel sheet 2. Created a custom class which is going to store the excel values 3. Created a method for storing values from excel sheet in to in-memory collection

--- EATestProject/EATestProject/Helpers/ExcelHelpers.cs
+++ EATestProject/EATestProject/Helpers/ExcelHelpers.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using ExcelDataReader;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EAAutoFramework.Helpers
 {
-    class ExcelHelpers
+    public class ExcelHelpers
     {
+        private static List<DataCollection> dataCol = new List<DataCollection>();
+
+        public static void PopulateInCollection(string fileName)
+        {
+            DataTable table = ExcelToDataTable(fileName);
+
+            //iterate through the rows and columns of the table
+            for(int row = 1; row <= table.Rows.Count; row++)
+            {
+                for (int col = 0; col < table.Columns.Count; col++)
+                {
+                    DataCollection dtTable = new DataCollection()
+                    {
+                        rowNumber = row,
+                        colName = table.Columns[col].ColumnName,
+                        colValue = table.Rows[row - 1][col].ToString()
+                    };
+                    //add all the details for each row
+                    dataCol.Add(dtTable);
+                }
+            }
+        }
+
+        private static DataTable ExcelToDataTable(string fileName)
+        {
+            //open file and return as Stream
+            FileStream stream = File.Open(fileName, FileMode.Open, FileAccess.Read);
+            //Createopenxmlreader via ExcelReaderFactory for .xlsx
+            IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream); 
+            //Set the first row as column name
+            excelReader.IsFirstRowAsColumnNames = true;
+            //Return as DataSet
+            DataSet result = excelReader.AsDataSet();
+            //Get all the Tables
+            DataTableCollection table = result.Tables;
+            //Store it in DataTable
+            DataTable resultTable = table["Sheet1"];
+
+            return resultTable;
+        }
+    }
+
+    public class DataCollection
+    {
+        public int rowNumber { get; set; }
+        public string colName { get; set; }
+        public string colValue { get; set; }
     }
 }
