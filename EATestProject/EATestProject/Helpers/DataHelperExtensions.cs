8ce623d7aeb48f9c75ac0b45d5d6b040c2cecffa
garrathjones <jones.garrath@gmail.com>
2021-07-22 21:08:53 +0100

    Added to the DataHelperExtension: methods to open & close an sql connection and one to execute a query

--- EATestProject/EATestProject/Helpers/DataHelperExtensions.cs
+++ EATestProject/EATestProject/Helpers/DataHelperExtensions.cs
@@ -1,12 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace EAAutoFramework.Helpers
 {
-    class DataHelperExtensions
+    public static class DataHelperExtensions
     {
+        //Open the connection
+        public static SqlConnection DBConnect(this SqlConnection sqlConnection, string connectionString)
+        {
+            try
+            {
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+                return sqlConnection;
+            }
+            catch (Exception e)
+            {
+                LogHelpers.Write("ERROR :: " + e.Message);
+            }
+
+            return null; 
+        }
+
+        //Close the connection
+        public static void DBClose(this SqlConnection sqlConnection)
+        {
+            try
+            {
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                LogHelpers.Write("ERROR :: " + e.Message);               
+            }
+        }
+
+        //Execute query
+        public static DataTable ExecuteQuery(this SqlConnection sqlConnection, string queryString)
+        {
+            DataSet dataSet;
+            try
+            {
+                //Check the state of the connection
+                if (sqlConnection == null || ((sqlConnection != null && (sqlConnection.State == ConnectionState.Closed 
+                    || sqlConnection.State == ConnectionState.Broken))))
+                {
+                    sqlConnection.Open();
+                }
+
+                SqlDataAdapter dataAdapter = new SqlDataAdapter();
+                dataAdapter.SelectCommand = new SqlCommand(queryString, sqlConnection);
+                dataAdapter.SelectCommand.CommandType = CommandType.Text;
+
+                dataSet = new DataSet();
+                dataAdapter.Fill(dataSet, "table");
+                sqlConnection.Close();
+                return dataSet.Tables["table"];
+            }
+            catch (Exception e)
+            {
+                dataSet = null;
+                sqlConnection.Close();
+                LogHelpers.Write("ERROR :: " + e.Message);
+                return null;
+            }
+            finally
+            {                
+                sqlConnection.Close();
+                dataSet = null;
+            }
+        }
     }
 }
